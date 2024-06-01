294d5ae8a9c380cd8cc11a54fd42a34971809177
Nemanja Krcmar <nk.nemanjakrcmar@gmail.com>
2024-06-01 19:37:09 +0200

    Finished commenting on the contract

--- solidity/Preferans.sol
+++ solidity/Preferans.sol
@@ -20,13 +20,13 @@ contract Preferans {
         uint256[] hand; 
         bool withdrawn;
     }
-
+    //Ruka koja je odigrana u partiji
     struct Trick {
         uint256[3] cards;
         address[3] players;
         uint256 leadSuit;
     }
-
+    // "igra" 
     struct GameCall {
         address player;
         GameType gameType;
@@ -65,11 +65,13 @@ contract Preferans {
     mapping(uint256 => uint256) cardSuit; 
     mapping(uint256 => uint256) cardValue; 
 
+    //Uslov pred f-ju da mogu samo igraci da je pozivaju
     modifier onlyPlayer() {
         require(msg.sender == players[0].addr || msg.sender == players[1].addr || msg.sender == players[2].addr, "Not a player");
         _;
     }
 
+    //Kreiranje pametnog ugovora i defisanje pocetnih parametara za igru
     constructor(address[3] memory playerAddresses, uint256 refeCount) payable {
         for (uint256 i = 0; i < 3; i++) {
             players[i] = Player({
@@ -99,13 +101,15 @@ contract Preferans {
         initializeCardMappings();
     }
 
+    //Generisanje karata
     function initializeCardMappings() internal {
         for (uint256 i = 0; i < 32; i++) {
-            cardSuit[i] = i / 8; // 4 suits, 8 cards each
-            cardValue[i] = i % 8; // Values from 0 to 7 within each suit
+            cardSuit[i] = i / 8; // 4 znaka, 8 karata za svaki znak
+            cardValue[i] = i % 8; // Vrednosti od 0 - 7 za svaku kartu (od 7 - Ace)
         }
     }
 
+    //Funkcija koja oznacava pocetak licitacije za igru
     function startBidding() external onlyPlayer {
         require(biddingStatus == BiddingStatus.NotStarted, "Bidding already started");
         biddingStatus = BiddingStatus.InProgress;
@@ -123,6 +127,7 @@ contract Preferans {
         shuffleAndDeal();
     }
 
+    //F-ja za generisanje spila i raspodelu karata ka igracima i u "kup"
     function shuffleAndDeal() internal {
         deck = new uint256[] (0) ;
         for (uint256 i = 0; i < 32; i++) {
@@ -141,6 +146,7 @@ contract Preferans {
         stock[1] = deck[31];
     }
 
+    //F-ja za mesanje karata
     function shuffleDeck() internal {
         for (uint256 i = 0; i < deck.length; i++) {
             uint256 n = i + uint256(keccak256(abi.encodePacked(block.timestamp))) % (deck.length - i);
@@ -150,6 +156,7 @@ contract Preferans {
         }
     }
 
+    //F-ja za definisanje najjace "igre" 
     function determineHighestGame() internal {
         GameType highestGame = GameType.None;
         address highestGameCaller = address(0);
@@ -165,6 +172,7 @@ contract Preferans {
         currentGame = highestGame;
     }
 
+    //Funkcija sa kojom se postavlja licitacija (dalje, broj ili "igra")
     function placeBid(uint256 bid, GameType gameType) external onlyPlayer {
         require(biddingStatus == BiddingStatus.InProgress, "Bidding not in progress");
         require(players[currentBidderIndex].addr == msg.sender, "Not your turn to bid");
@@ -183,7 +191,7 @@ contract Preferans {
                 advanceBidder();
             }
             return;
-        } else {
+        } else { //Obicna licitacija
             require(bid > highestBid && bid == highestBid + 1, "Cant overbid");
             require(bid <= 7, "Bid cannot exceed 7");
 
@@ -212,6 +220,7 @@ contract Preferans {
         }
     }
 
+    //F-ja za oznacavanje ko sledeci treba da licitira
     function advanceBidder() internal {
         do {
             currentBidderIndex = nextBidderIndex;
@@ -219,6 +228,7 @@ contract Preferans {
         } while (players[currentBidderIndex].status == PlayerStatus.Out);
     }
 
+    //F-ja koja vraca koliko trenutno ima aktivnih igraca
     function activePlayersCount() internal view returns (uint256) {
         uint256 count = 0;
         for (uint256 i = 0; i < 3; i++) {
@@ -229,6 +239,7 @@ contract Preferans {
         return count;
     }
 
+    //F-ja koja vraca da li su svi glasali ako postoji barem jedna proglasena "igra"
     function atLeastOneGameCalledAndOthersBidded() internal view returns (bool) {
         bool gameCalledByOnePlayer = gameCalls.length > 0;
         bool allOthersBidded = true;
@@ -243,15 +254,15 @@ contract Preferans {
         return gameCalledByOnePlayer && allOthersBidded;
     }
 
-
+    //F-ja koja oznacava kraj licitacija i vrsi prelaz na sledeci tok ruke
     function endBidding() internal {
         require(biddingStatus == BiddingStatus.InProgress, "Bidding not in progress");
         biddingStatus = BiddingStatus.Finished;
 
-        if (activePlayersCount() == 0) {
+        if (activePlayersCount() == 0) { //Ako su svi rekli dalje
             recordRefe();
             resetGame();
-        } else {
+        } else { //Ako je prozvana igra
             if (gameCalls.length > 0) {
                 determineHighestGame();
             } else {
@@ -263,7 +274,7 @@ contract Preferans {
                 }
             }
 
-            if (!gameCalled) {
+            if (!gameCalled) { //Ako je normalna licitacija
                 for (uint256 i = 0; i < 3; i++) {
                     if (players[i].addr == winningBidder) {
                         players[i].hand.push(stock[0]);
@@ -278,11 +289,12 @@ contract Preferans {
         }
     }
 
+    //F-ja sa kojom se proziva igra
     function declareGame(GameType game) external onlyPlayer {
         require(biddingStatus == BiddingStatus.Finished, "Bidding not finished");
         require(msg.sender == winningBidder, "Only the winning bidder can declare the game");
         currentGame = game;
-        for(uint i = 0; i < 3; i++) {
+        for(uint i = 0; i < 3; i++) { //Ako trenutni igrac ima refe
             if(players[i].addr == msg.sender && players[i].refeCount > 0){
                 kontraMultiplier *= 2;
                 players[i].refeCount--;
@@ -290,6 +302,7 @@ contract Preferans {
         }
     }
 
+    //F-ja koja izbacuje karte iz ruku igraca koji je pobedio u licitaciji
     function removeCardsAfterAddingStockCards(uint256 card1, uint256 card2) external onlyPlayer {
         require(biddingStatus == BiddingStatus.Finished, "Bidding not finished");
         require(msg.sender == winningBidder, "Only the winning bidder can declare the game");
@@ -314,6 +327,7 @@ contract Preferans {
         }
     }
 
+    //F-ja gde ostali igraci biraju da li ce da prate igru ili ne
     function followGame(bool follow) external onlyPlayer {
         require(biddingStatus == BiddingStatus.Finished, "Bidding not finished");
         for (uint256 i = 0; i < 3; i++) {
@@ -323,6 +337,7 @@ contract Preferans {
         }
     }
 
+    //F-ja sa kojom se oznacava da igrac koji je odlucio da ne prati igru pude pozvan da igra
     function callToGame() external onlyPlayer {
         require(biddingStatus == BiddingStatus.Finished, "Bidding not finished");
         for (uint256 i = 0; i < 3; i++) {
@@ -338,6 +353,7 @@ contract Preferans {
         }
     }
 
+    //F-ja sa kojom igrac koji prati igru moze da pozove kontru
     function declareKontra() external onlyPlayer {
         require(biddingStatus == BiddingStatus.Finished, "Bidding not finished");
         for (uint256 i = 0; i < 3; i++) {
@@ -355,6 +371,7 @@ contract Preferans {
         }
     }
 
+    //F-ja sa kojom igrac koji vodi igru ako je data kontra moze da pozove rekontru
     function declareReKontra() external onlyPlayer() {
         require(biddingStatus == BiddingStatus.Finished, "Bidding not finished");
         require(winningBidder == msg.sender, "Only Bidding winner can recounter");
@@ -362,6 +379,7 @@ contract Preferans {
         kontraMultiplier *= 2;
     }
 
+    //F-ja sa kojom igrac odigrava kartu iz ruke
     function playCard(uint256 card) external onlyPlayer {
         require(biddingStatus == BiddingStatus.Finished, "Game not started yet");
         require(currentTrick.players[currentTurn] == address(0), "Player has already played this trick");
@@ -387,6 +405,7 @@ contract Preferans {
         }
     }
 
+    //F-ja sa kojom se karta koja je odigrana izbacuje iz ruke
     function removeCardFromHand(address playerAddr, uint256 card) internal {
         for (uint256 i = 0; i < 3; i++) {
             if (players[i].addr == playerAddr) {
@@ -402,6 +421,7 @@ contract Preferans {
         }
     }
 
+    //F-ja sa kojom se pronalazi karta koja zeli da se odigra u ruci
     function findCardIndex(uint256[] memory hand, uint256 card) internal pure returns (uint256) {
         for (uint256 i = 0; i < hand.length; i++) {
             if (hand[i] == card) {
@@ -411,6 +431,7 @@ contract Preferans {
         return hand.length;
     }
 
+    //F-ja koja proverava validnost odigrane karte
     function isValidCardPlay(uint256 card) internal view returns (bool) {
         uint256 leadSuit = currentTrick.leadSuit;
 
@@ -422,25 +443,26 @@ contract Preferans {
             }
         }
 
-        if (hasLeadSuit) {
+        if (hasLeadSuit) { //Ako ima vodecu kartu
             return cardSuit[card] == leadSuit;
         }
 
         bool hasTrump = false;
         for (uint256 i = 0; i < players[currentPlayer].hand.length; i++) {
-            if (isTrump(cardSuit[players[currentPlayer].hand[i]])) {
+            if (isTrump(cardSuit[players[currentPlayer].hand[i]])) { //Ako ima aduta
                 hasTrump = true;
                 break;
             }
         }
 
-        if (hasTrump && currentGame != GameType.Misere && currentGame != GameType.NoTrump) {
+        if (hasTrump && currentGame != GameType.Misere && currentGame != GameType.NoTrump) { 
             return isTrump(cardSuit[card]);
         }
 
         return true;
     }
 
+    //F-ja za proveru da li je znak adut
     function isTrump(uint256 suit) internal view returns (bool) {
         if (currentGame == GameType.Spades && suit == 0) {
             return true;
@@ -454,6 +476,7 @@ contract Preferans {
         return false;
     }
 
+    //F-ja za razresavanje ruke (tri karte koje su odigrane na tabli)
     function resolveTrick() internal {
         uint256 winningCardIndex = 0;
         for (uint256 i = 1; i < 3; i++) {
@@ -486,6 +509,7 @@ contract Preferans {
         }
     }
 
+    //F-ja za uporedjivanje jacine karata koje su na tabli
     function beats(uint256 card1, uint256 card2) internal view returns (bool) {
         if (cardSuit[card1] == cardSuit[card2]) {
             return cardValue[card1] > cardValue[card2];
@@ -499,6 +523,7 @@ contract Preferans {
         return false;
     }
 
+    //F-ja sa kojom se za svakog igraca na osnovu igre racunaju bule i supe
     function calculateScores() internal {
         uint256 baseScore;
         uint8 gameWasCalled = (gameCalled ? 2 : 0);
@@ -573,6 +598,7 @@ contract Preferans {
         }
     }
 
+    //F-ja koja vraca trenutne krajnje rezultate igraca u partiji
     function getCurrentScores() external view returns (int256[3] memory) {
         int256[3] memory scores;
         for (uint256 i = 0; i < 3; i++) {
@@ -583,6 +609,7 @@ contract Preferans {
         return scores;
     }
 
+    //F-ja za racunanje trenutnih supa za svakog igraca
     function getCurrentSupa() external view returns (uint256[3][2] memory) {
         uint256[3][2] memory supas;
         for (uint256 i = 0; i < 3; i++) {
@@ -592,6 +619,7 @@ contract Preferans {
         return supas;
     }
 
+    //F-ja za racunanje trenutnih bula za svakog igraca
     function getCurrentBula() external view returns (int256[3] memory) {
         int256[3] memory bulas;
         for (uint256 i = 0; i < 3; i++) {
@@ -600,13 +628,17 @@ contract Preferans {
         return bulas;
     }
 
+    //F-ja sa kojom se belezi refe
     function recordRefe() internal {
-        for (uint256 i = 0; i < 3; i++) {
-            players[i].refeCount++;
+        if(totalRefes > 0){
+            for (uint256 i = 0; i < 3; i++) {
+                players[i].refeCount++;
+            }
+            totalRefes--;
         }
-        totalRefes--;
     }
 
+    //F-ja sa kojom se resetuju parametri za novu ruku
     function resetGame() internal {
         currentGame = GameType.None;
         biddingStatus = BiddingStatus.NotStarted;
@@ -627,10 +659,12 @@ contract Preferans {
         }
     }
 
+    //F-ja sa kojom se obavestava ko sledeci igra za dato bacanje
     function nextTurn() internal {
         currentPlayer = (currentPlayer + 1) % 3;
     }
 
+    //F-ja sa kojom se isplacuju svi igraci nakon partije
     function withdraw() external payable onlyPlayer() {
         int256 difference = 0;
         require(players[0].addr == msg.sender && players[0].withdrawn == false || 
